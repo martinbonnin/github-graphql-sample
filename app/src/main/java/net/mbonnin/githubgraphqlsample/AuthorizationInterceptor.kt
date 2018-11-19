bb45016ffcaacf52cc85c6267cfbcd783dd5457d
Martin Bonnin <martin.bonnin@dailymotion.com>
2018-11-19 19:16:03 +0100

    added an Authorization Interceptor

---
+++ app/src/main/java/net/mbonnin/githubgraphqlsample/AuthorizationInterceptor.kt
@@ -0,0 +1,17 @@
+package net.mbonnin.githubgraphqlsample
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthorizationInterceptor(val token: String): Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+
+        val newRequest = request.newBuilder()
+            .addHeader("Authorization", "Bearer ${token}")
+            .build()
+
+        return chain.proceed(newRequest)
+    }
+
+}
